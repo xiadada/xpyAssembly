f7590ee6707237234eba2ac6d51b1f1d81d89e08
xiadada <1185046390@qq.com>
2018-02-07 15:58:11 +0800

    组件相关

---
+++ gulp/tasks/tpl.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+	tmodjs = require('gulp-tmod');
+ 
+gulp.task('tpl', function(){
+  return gulp.src(['src/tpl/**/*.*'])
+		.pipe(tmodjs({
+			templateBase: 'src/tpl',
+			combo: true,
+			type: 'amd',
+			// helpers: 'src/js/common/helper.js',
+			output: 'build/assets/template'
+	}));
+});
